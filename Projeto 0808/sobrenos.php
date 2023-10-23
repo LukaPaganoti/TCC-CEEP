99ed79874f6aaa22a47687e46bd4fe8b5401633c
Luiza Maria Paganoti Farias <124787994+OrthoShrouds@users.noreply.github.com>
2023-10-23 19:29:31 -0300

    Add files via upload

---
+++ Projeto 0808/sobrenos.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StudiosBen - Artesanato Geek e Otaku</title>
+    <style>
+        /* Estilos gerais */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        /* Estilos para o cabeçalho */
+        header {
+            background-color: #be18bb;
+            color: #fff;
+            padding: 20px 0;
+            text-align: center;
+        }
+
+        header h1 {
+            font-size: 36px;
+            margin: 0;
+        }
+
+        /* Estilos para o conteúdo principal */
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+            flex: 1; /* Preenche o espaço restante na tela */
+        }
+
+        h2 {
+            color: #8a2be2;
+        }
+
+        p {
+            margin-bottom: 20px;
+            font-size: 18px; /* Aumenta o tamanho da fonte */
+        }
+
+        /* Estilos para a caixa de pesquisa */
+
+        /* Rodapé */
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <h1>StudiosBen - Artesanato Geek e Otaku</h1>
+    </header>
+
+    <div class="container">
+        <h2>Bem-vindo ao StudiosBen!</h2>
+        <br>
+        <p>StudiosBen é um e-commerce com foco na comunidade geek e otaku</p>
+        <p>oferecendo produtos artesanais e exclusivos para todos os gostos!</p>
+        <br>
+        <p>Encontre produtos únicos e feitos com paixão para os fãs de cultura pop, anime e videogames.</p>
+    </div>
+
+    <footer>
+        <p>&copy; 2023 StudiosBen</p>
+    </footer>
+</body>
+</html>
